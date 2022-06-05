eff3ca9801c5bd045a6d6e8a55817d3f151b07b6
Paladic <nak78nik@gmail.com>
2022-06-05 19:23:57 +0300

    бжжж

---
+++ BotForSimpleIdeas/Commands/Command.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Interactions;
+using Fergun.Interactive;
+
+namespace BotForSimpleIdeas.Commands;
+
+public class Command : InteractionModuleBase<SocketInteractionContext>
+{
+    public InteractiveService Interactive { get;}
+    
+    public Command(InteractiveService interactive)
+    {
+        Interactive = interactive;
+    }
+
+    [SlashCommand("симпл-разбанить", "разблокирует всех юзеров")]
+    [DefaultMemberPermissions(GuildPermission.Administrator)]
+    [EnabledInDm(false)]
+    public async Task CheckUserAwardsAsync()
+    {
+        await DeferAsync();
+        var bans = Context.Guild.GetBansAsync();
+        await foreach (var ban in bans)
+        {
+            foreach (var cur in ban)
+            {
+                await Context.Guild.RemoveBanAsync(cur.User);
+                await Context.Channel.SendMessageAsync($"{cur.User} разбанен");
+                await Task.Delay(1 * 1000);
+            }
+        }
+    }
+    
+
+}
